f64b25942b8bec87e00c1d26f2c160c56203ff26
DHARANI213 <43335533+DHARANI213@users.noreply.github.com>
2019-02-13 14:29:22 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("NUMBER");
+scanf("%d",&a);
+if(a==3)
+{
+printf("POSITIVE");
+}
+else
+{
+printf("");
+}
+getch();
+}
