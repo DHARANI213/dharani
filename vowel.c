65ba9f7840db5358f955932725e4da025d730859
DHARANI213 <43335533+DHARANI213@users.noreply.github.com>
2019-02-20 13:49:19 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char d;
+clrscr();
+printf("Enter the character");
+scanf("%s",&d);
+if(d='a')
+{
+printf("vowel");
+}
+else
+{
+printf("");
+)
+getch();
+}
+
